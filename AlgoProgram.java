7a5fc8c23b576e5991eefa07c3c3f485a71333db
rrajput7461 <rohitr_ug_22@mech.nits.ac.in>
2025-12-17 17:57:19 +0530

    Binary Search

--- AlgoProgram.java
+++ AlgoProgram.java
@@ -1,14 +1,17 @@
+import java.util.Arrays;
 public class AlgoProgram {
-    // Insertion Sort for String array
-    public static void insertionSort(String[] arr) {
-        for (int i = 1; i < arr.length; i++) {
-            String key = arr[i];
-            int j = i - 1;
-            while (j >= 0 && arr[j].compareToIgnoreCase(key) > 0) {
-                arr[j + 1] = arr[j];
-                j--;
-            }
-            arr[j + 1] = key;
+    public static boolean search(String[] words, String key) {
+        Arrays.sort(words);
+        int low = 0, high = words.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (words[mid].equalsIgnoreCase(key))
+                return true;
+            else if (words[mid].compareToIgnoreCase(key) < 0)
+                low = mid + 1;
+            else
+                high = mid - 1;
         }
+        return false;
     }
 }
--- Main.java
+++ Main.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter words (comma separated): ");
         String[] words = sc.nextLine().split(",");
-        AlgoProgram.insertionSort(words);
-        System.out.println("Sorted words:");
-        for (String word : words) {
-            System.out.print(word + " ");
-        }
+        System.out.print("Enter word to search: ");
+        String key = sc.nextLine();
+        if (AlgoProgram.search(words, key))
+            System.out.println("Word FOUND");
+        else
+            System.out.println("Word NOT FOUND");
         sc.close();
     }
 }
