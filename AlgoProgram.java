e420f883b5177608e7f037d147afa10738d6db4b
rrajput7461 <rohitr_ug_22@mech.nits.ac.in>
2025-12-17 18:49:35 +0530

    Prime Number in Range of 0 to 1000

--- AlgoProgram.java
+++ AlgoProgram.java
@@ -1,17 +1,11 @@
-import java.util.Arrays;
 public class AlgoProgram {
-    public static boolean search(String[] words, String key) {
-        Arrays.sort(words);
-        int low = 0, high = words.length - 1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (words[mid].equalsIgnoreCase(key))
-                return true;
-            else if (words[mid].compareToIgnoreCase(key) < 0)
-                low = mid + 1;
-            else
-                high = mid - 1;
+    public static boolean isPrime(int n) {
+        if (n <= 1)
+            return false;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0)
+                return false;
         }
-        return false;
+        return true;
     }
 }
--- Main.java
+++ Main.java
@@ -1,15 +1,10 @@
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter words (comma separated): ");
-        String[] words = sc.nextLine().split(",");
-        System.out.print("Enter word to search: ");
-        String key = sc.nextLine();
-        if (AlgoProgram.search(words, key))
-            System.out.println("Word FOUND");
-        else
-            System.out.println("Word NOT FOUND");
-        sc.close();
+        System.out.println("Prime numbers between 0 and 1000:");
+        for (int i = 0; i <= 1000; i++) {
+            if (AlgoProgram.isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
