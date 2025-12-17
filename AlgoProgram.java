f52aed195559325501f34fa90b0caa9958501682
rrajput7461 <rohitr_ug_22@mech.nits.ac.in>
2025-12-17 17:55:32 +0530

    Insertion Sort

--- AlgoProgram.java
+++ AlgoProgram.java
@@ -1,17 +1,14 @@
-import java.util.Arrays;
 public class AlgoProgram {
-    public static boolean search(String[] words, String key) {
-        Arrays.sort(words);
-        int low = 0, high = words.length - 1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (words[mid].equalsIgnoreCase(key))
-                return true;
-            else if (words[mid].compareToIgnoreCase(key) < 0)
-                low = mid + 1;
-            else
-                high = mid - 1;
+    // Insertion Sort for String array
+    public static void insertionSort(String[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            String key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j].compareToIgnoreCase(key) > 0) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
         }
-        return false;
     }
 }
